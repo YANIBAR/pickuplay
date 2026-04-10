a0d1ba8e00e09881d939be2b4d068c4be8fd1308
youssef <youssef@Mac.lan>
2026-04-10 14:19:50 -0500

    onboarding improvmemt

--- src/screens/onboarding/index.tsx
+++ src/screens/onboarding/index.tsx
@@ -1,147 +1,88 @@
-import React, { useState, useRef } from 'react';
-import { View, Text, ScrollView, TextInput, TouchableOpacity } from 'react-native';
-import { Button } from '@components';
-import { useNavigation } from '@react-navigation/native';
+import Onboarding from 'react-native-onboarding-swiper';
+import { Image, View, Text } from 'react-native';
+import { useFocusEffect, useNavigation } from '@react-navigation/native';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 import { useTranslation } from 'react-i18next';
 import styles from './styles';
-import AsyncStorage from '@react-native-async-storage/async-storage';
+import { Button } from '@components';
+import { COLORS } from '@constants';
 
 type Nav = {
   navigate: (value: string) => void;
 };
 
-interface FormData {
-  ageGroup: string;
-  respondingAs: string;
-  numberOfChildren: number;
-  games: string[];
-  barriers: string[];
-  attractiveFeatures: string[];
-  suggestions: string;
-}
+const onboardingPages = [
+  {
+    backgroundColor: COLORS.white,
+    image: <Image source={require('../../assets/illustrations/onboarding/step1.png')} style={{ width: 200, height: 400 }} />,
+    title: 'Find Your Game',
+    subtitle: 'Filter by city, sport, and day to quickly discover games near you. \nPick what fits your schedule and jump in.',
+  },
+  {
+    backgroundColor: COLORS.white,
+    image: <Image source={require('../../assets/illustrations/onboarding/step2.png')} style={{ width: 200, height: 200 }} />,
+    title: 'Real-time Notifications',
+    subtitle: 'Tap Join, choose players, bring a guest, and apply a discount code if you have one. \nYou\'re in instantly.',
+  },
+  {
+    backgroundColor: COLORS.white,
+    image: <Image source={require('../../assets/illustrations/onboarding/step3.png')} style={{ width: 200, height: 200 }} />,
+    title: 'Never Miss an Update',
+    subtitle: 'Get notified if the game is updated, canceled, or full. \nReceive reminders before kickoff so you`re always on time.',
+  },
+  {
+    backgroundColor: COLORS.white,
+    image: <Image source={require('../../assets/illustrations/onboarding/onboarding1.png')} style={{ width: 200, height: 200 }} />,
+    title: 'Play & Connect',
+    subtitle: 'Meet new players, enjoy the game, and bring positive vibes. \n Pickuplay is all about fun, community, and good energy ⚽',
+  },
+];
 
-const OnBoardingForm = () => {
+export default function OnboardingScreen() {
+
   const { t } = useTranslation(); 
   const { navigate } = useNavigation<Nav>();
-  const [currentSlide, setCurrentSlide] = useState(0);
-  const scrollViewRef = useRef<ScrollView>(null);
-  
-  const [formData, setFormData] = useState<FormData>({
-    ageGroup: '',
-    respondingAs: '',
-    numberOfChildren: 0,
-    games: [],
-    barriers: [],
-    attractiveFeatures: [],
-    suggestions: ''
-  });
-
-  const slides = [
-    {
-      id: 1,
-      title: "Find Your Game",
-      description:
-        "Use filters like sport, city, and date to discover games near you.",
-      icon: "🔍"
-    },
-    {
-      id: 2,
-      title: "Check Game Details",
-      description:
-        "View location, time, players, and level before joining.",
-      icon: "📋"
-    },
-    {
-      id: 3,
-      title: "Join Easily",
-      description:
-        "Pick number of players, apply promo code, and confirm instantly.",
-      icon: "⚡"
-    },
-    {
-      id: 4,
-      title: "Play & Enjoy",
-      description:
-        "Meet players, have fun, and enjoy your game with Pickuplay.",
-      icon: "🎉"
-    }
-  ];
-
-  const currentSlideData = slides[currentSlide];
-
-
-  const handleNext = async () => {
-  if (isLastSlide) {
-    await AsyncStorage.setItem("hasLaunched", "true");
-    navigate("welcome");
-    return;
-  }
-  setCurrentSlide(currentSlide + 1);
-  scrollViewRef.current?.scrollTo({ y: 0, animated: true });
-};
-
-  const handlePrevious = () => {
-    if (currentSlide > 0) {
-      setCurrentSlide(currentSlide - 1);
-      scrollViewRef.current?.scrollTo({ y: 0, animated: true });
-    }
+  const handleDone = async () => {
+    await AsyncStorage.setItem('hasLaunched', 'true');
+    navigate('welcome');
   };
-
-  const renderSlide = () => {
-    const slide = currentSlideData;
-    
-    return (
-      <View style={styles.slideContainer}>
-        <Text style={styles.slideNumber}>
-          {currentSlide + 1}
-        </Text>
-        
-        <View style={styles.slideTitleContainer}>
-          <Text style={styles.slideTitleFr}>{slide.title}</Text>
-        </View>
-        <Text style={[ styles.optionText]}>
-          {`${slide.description}`}
-        </Text>
-      </View>
-    );
-  };
-
-  const isFirstSlide = currentSlide === 0;
-  const isLastSlide = currentSlide === slides.length - 1;
 
   return (
-    <View style={styles.container}>
-      <ScrollView 
-        ref={scrollViewRef}
-        contentContainerStyle={styles.scrollContent}
-        showsVerticalScrollIndicator={false}
-      >
-        {renderSlide()}
-      </ScrollView>
-      
-      <View style={styles.buttonsContainer}>
+    
+    <Onboarding
+      pages={onboardingPages}
+      bottomBarColor={COLORS.white}
+      onDone={handleDone}
+      onSkip={handleDone}
+      bottomBarHighlight={false}
+      showSkip
+      showNext
+      showDone
+      titleStyles={{ fontFamily: 'YourCustomFont-Bold', fontSize: 24 }}
+      subTitleStyles={{ fontFamily: 'YourCustomFont-Regular', fontSize: 16 }}
+      containerStyles={{ paddingBottom: 40 }}
+      // You can also customize buttons and dots here
+       NextButtonComponent={({ isLight, ...NextButtonProps }) => (
         <Button 
-          onPress={handlePrevious}
-          title={t('onboarding.previousButton')}
-          disabled={isFirstSlide}
+        onPress={NextButtonProps.onPress}
+          title={t('onboarding.nextButton')}
           style={[
             styles.navigationButton,
-            styles.previousButton,
-            isFirstSlide && styles.hiddenButton
+            styles.nextButton
           ]}
         />
-        
+      )}
+      SkipButtonComponent={({ isLight, ...SkipButtonProps }) => (
         <Button 
-          onPress={handleNext}
-          title={isLastSlide 
-            ? t('onboarding.finishButton') 
-            : t('onboarding.nextButton')
-          }
-          style={[styles.navigationButton, styles.nextButton]}
-        />
-      </View>
-    </View>
-  );
-};
 
-export default OnBoardingForm;
+        onPress={SkipButtonProps.onPress}
+          title={t('onboarding.skipButton')}
+          style={[
+            styles.navigationButton,
+            styles.nextButton
+          ]}
+        />
+      )}
+    />
+  );
+}
